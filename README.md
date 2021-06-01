5f5b8afa33e924a064e2b26fe0ea905e2f206cb9
takashibaken <57272286+takashibaken@users.noreply.github.com>
2021-05-31 23:05:00 -0300

    versão 1.0

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hbs-uti
  Página da equipe de UTI do Hospital Baía Sul
+
+Versão 1
+- página dedicada à equipe de UTI do hospital Baía Sul.
+
+Versão 2
+- expansão para o hospital Baía Sul.
+
+Versão 3
+- expansão para a rede de hospitais de Florianópolis.
+
+Versão 4
+- expansão para clínicas, como um software utilitário.
--- docs/scripts/script_antrop.js
+++ docs/scripts/script_antrop.js
@@ -1,15 +1,34 @@
+
 function calcular() {
-    let altura = document.getElementById('altura');
-    let peso = document.getElementById('peso');
-    let res = document.getElementById('res');
-    let masc = document.getElementsByName('masc');
-    let femin = document.getElementsByName('femin');
+       
+    var altura = document.getElementById('box-altura');
+    var peso = document.getElementById('box-peso');
+    var masc = document.getElementById('masc');
+    var femin = document.getElementById('femin');
     
-    let a1 = Number(altura.value);
-    let p1 = Number(peso.value);
-
-    let pid_man = ((a1 - 152.4)*0.91 + 50);
-    let pid_wom = ((a1 - 152.4)*0.91 + 45.5);
-
+    var height = Number(altura.value).toFixed(0);
+    var weight = Number(peso.value).toFixed(0);
     
+       
+    if (document.getElementById('femin').checked) {
+            var weightIdeal = ((height - 152.4)*0.91 + 45.5).toFixed(0);
+            var vm4Ideal = (weightIdeal * 4).toFixed(0);
+            var vm6Ideal = (weightIdeal * 6).toFixed(0);
+
+        } else if (document.getElementById('masc').checked) {
+            var weightIdeal = ((height - 152.4)*0.91 + 50).toFixed(0);
+            var vm4Ideal = (weightIdeal * 4).toFixed(0);
+            var vm6Ideal = (weightIdeal * 6).toFixed(0);
+
+        }
+
+
+        
+
+    var evol = document.getElementById('evolp2');
+    evol.innerHTML = `Peso real: ${weight} kg || Altura: ${height} cm || Peso ideal VM: ${weightIdeal} kg || Volume corrente (4 - 6 mL/kg): ${vm4Ideal} - ${vm6Ideal} mL`;
+
+        
+  
+  
 }
