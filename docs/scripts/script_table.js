3b7b3d90eb261b61203170e5df7bc4888dd8d881
takashibaken <57272286+takashibaken@users.noreply.github.com>
2021-06-01 22:59:00 -0300

    versao2.0

---
+++ docs/scripts/script_table.js
@@ -0,0 +1,26 @@
+var formPeso = document.getElementById("form-peso");
+var vazaoFenta = document.getElementById("vazao-fenta");
+var vazaoMida = document.getElementById("vazao-mida");
+var vazaoPropo = document.getElementById("vazao-propo");
+
+function calcFenta() {
+    var peso = Number(formPeso.value);    
+    var fenta = Number(vazaoFenta.value);
+    var doseFenta = (fenta*20)/peso;
+    this.resFenta.innerHTML = doseFenta.toFixed(2);
+
+}
+
+function calcMida() {
+    var peso = Number(formPeso.value);    
+    var mida = Number(vazaoMida.value);
+    var doseMida = (mida*2)/peso;
+    this.resMida.innerHTML = doseMida.toFixed(2);
+}
+
+function calcPropo() {
+    var peso = Number(formPeso.value);    
+    var propo = Number(vazaoPropo.value);
+    var dosePropo = (propo*10)/peso;
+    this.resPropo.innerHTML = dosePropo.toFixed(2);
+}
